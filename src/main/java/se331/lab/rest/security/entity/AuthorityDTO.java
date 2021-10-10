7d695aaa5957b5356eae28e6086c54c29568d950
munlikaCh <munlika_ch@cmu.ac.th>
2021-10-11 00:10:41 +0700

    6.8

---
+++ src/main/java/se331/lab/rest/security/entity/AuthorityDTO.java
@@ -0,0 +1,14 @@
+package se331.lab.rest.security.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorityDTO {
+    String name;
+}
