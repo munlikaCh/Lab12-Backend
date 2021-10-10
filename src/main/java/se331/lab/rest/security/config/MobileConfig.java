6dc6798621466664c20a099ffe207fa349e2ff6b
munlikaCh <munlika_ch@cmu.ac.th>
2021-10-10 20:25:27 +0700

    1.4+1.5

--- src/main/java/se331/lab/rest/security/config/MobileConfig.java
+++ src/main/java/se331/lab/rest/security/config/MobileConfig.java
@@ -13,23 +13,27 @@ import java.util.List;
 
 @Configuration
 public class MobileConfig implements WebMvcConfigurer {
-    @Autowired
-    DeviceResolverHandlerInterceptor deviceResolverHandlerInterceptor;
+    @Bean
+    public DeviceResolverHandlerInterceptor deviceResolverHandlerInterceptor() {
+        return new DeviceResolverHandlerInterceptor();
+    }
 
 
-    @Autowired
-    DeviceHandlerMethodArgumentResolver deviceHandlerMethodArgumentResolver;
+    @Bean
+    public DeviceHandlerMethodArgumentResolver deviceHandlerMethodArgumentResolver() {
+        return new DeviceHandlerMethodArgumentResolver();
+    }
 
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(deviceResolverHandlerInterceptor);
+        registry.addInterceptor(deviceResolverHandlerInterceptor());
     }
 
     @Override
     public void addArgumentResolvers(
             List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(deviceHandlerMethodArgumentResolver);
+        argumentResolvers.add(deviceHandlerMethodArgumentResolver());
     }
 
 }
